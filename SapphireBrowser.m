ceadbec0e12cf4d987561c67378bb1697c5c42c8
gbooker <gbooker@7e1782f7-7b3f-45d9-b215-e5100ed717cc>
2007-07-07 03:31:40 +0000

    * Comments  * A small code cleanup
    git-svn-id: https://appletv.nanopi.net/svn/trunk@139 7e1782f7-7b3f-45d9-b215-e5100ed717cc

--- SapphireBrowser.m
+++ SapphireBrowser.m
@@ -35,6 +35,7 @@
 - (BRTextLayer *)gimmieShow;
 @end
 
+/*Private variables access, but only on BR 1.0; not used otherwise*/
 @implementation BRTVShowsSortControl (bypassAccess)
 - (BRTVShowsSortSelectorStateLayer *)gimmieDate
 {
@@ -68,27 +69,51 @@
 
 @implementation SapphireBrowser
 
+/*!
+ * @brief Replace the text in a control with a string
+ *
+ * @param control The control on which to change the text
+ * @param str The text to put into tho control
+ */
 - (void)replaceControlText:(BRTextLayer *)control withString:(NSString *)str
 {
+	/*Create a mutable copy of the control's attributed string*/
 	NSMutableAttributedString  *dateString = [[control attributedString] mutableCopy];
 	[dateString replaceCharactersInRange:NSMakeRange(0, [dateString length]) withString:str];
+	/*Set the new string and we are done with it*/
 	[control setAttributedString:dateString];
 	[dateString release];
 }
 
+/*!
+ * @brief Create a new normal browser
+ *
+ * @param scene The scene
+ * @param meta The metadata for the directory to browse
+ * @return The Browser
+ */
 - (id) initWithScene: (BRRenderScene *) scene metaData: (SapphireDirectoryMetaData *)meta
 {
 	return [self initWithScene:scene metaData:meta predicate:NULL];
 }
 
+/*!
+ * @brief Creates the mode control in a compatible way
+ *
+ * @param scene The scene
+ * @param names The names in the menu
+ */
 - (void)createModeControlWithScene:(BRRenderScene *)scene names:(NSArray *)names
 {
+	/*Check for the new way to do this*/
 	Class modeClass = NSClassFromString(@"BRSegmentedSortControl");
 	if(modeClass != nil)
+		/*Use the new method*/
 		//Ignore this warning if compiling with backrow 1.0
 		modeControl = [[modeClass alloc] initWithScene:scene segmentNames:names selectedSegment:0];
 	else
 	{
+		/*Hack in the old way*/
 		modeControl = [[BRTVShowsSortControl alloc] initWithScene:scene state:1];
 		NSString *name1 = [names objectAtIndex:0];
 		NSString *name2 = [names objectAtIndex:1];
@@ -99,6 +124,14 @@
 	}
 }
 
+/*!
+ * @brief Creates a new predicated browser
+ *
+ * @param scene The scene
+ * @praam meta The metadata for the directory to browse
+ * @param newPredicate The predicate to use
+ * @return The Browser
+ */
 - (id) initWithScene: (BRRenderScene *) scene metaData: (SapphireDirectoryMetaData *)meta predicate:(SapphirePredicate *)newPredicate;
 {
 	if ( [super initWithScene: scene] == nil ) return ( nil );
@@ -109,6 +142,7 @@
 	[metaData setDelegate:self];
 	predicate = [newPredicate retain];
 
+	/*Create the mode menu*/
 	NSArray *names = [NSArray arrayWithObjects:
 		BRLocalizedString(@"Select", @"Select Menu Item"),
 		BRLocalizedString(@"Mark File", @"Mark File Menu Item"),
@@ -122,30 +156,43 @@
 	return ( self );
 }
 
+/*!
+ * @brief Override the layout
+ */
 - (void)_doLayout
 {
 	[super _doLayout];
 	NSRect listFrame = [[_listControl layer] frame];
+	/*Position the mode menu below the list*/
 	NSRect modeRect;
 	modeRect.size = [modeControl preferredSizeForScreenHeight:[self masterLayerFrame].size.height];
 	modeRect.origin.y = listFrame.origin.y * 1.5f;
 	modeRect.origin.x = (listFrame.size.width - modeRect.size.width)/2 + listFrame.origin.x;
+	/*Shrink the list to make room for the mode*/
 	listFrame.size.height -= listFrame.origin.y;
 	listFrame.origin.y *= 2;
 	[[_listControl layer] setFrame:listFrame];
 	[modeControl setFrame:modeRect];
 }
 
+/*!
+ * @brief Reload the display
+ */
 - (void)reloadDirectoryContents
 {
+	/*Remove the dividers*/
 	BRListControl *list = [self list];
 	[list removeDividerAtIndex:dirCount];
 	[list removeDividerAtIndex:[_names count] - 1];
+	/*Tell the metadata to get new data*/
 	[metaData reloadDirectoryContents];
+	/*Flush our cache*/
 	[_names removeAllObjects];
 	[items removeAllObjects];
+	/*Check predicates*/
 	if(predicate == NULL)
 	{
+		/*No filtering, so just set up the dirs and files into names and the counts*/
 		NSArray *dirs = [metaData directories];
 		[_names addObjectsFromArray:dirs];
 		NSArray *files = [metaData files];
@@ -155,34 +202,47 @@
 	}
 	else
 	{
+		/*Filter the dirs and files into names and counts*/
 		NSArray *dirs = [metaData predicatedDirectories:predicate];
 		[_names addObjectsFromArray:dirs];
 		NSArray *files = [metaData predicatedFiles:predicate];
 		[_names addObjectsFromArray:files];
+		/*Put in the menu for rescan if fast switching enabled*/
 		if([[SapphireSettings sharedSettings] fastSwitching])
 			[_names addObject:BRLocalizedString(@"< Scan for new files >", @"Conduct a scan of the directory for new files")];
 		dirCount = [dirs count];
 		fileCount = [files count];
 	}
+	/*Init the cache*/
 	int i=0, count=[_names count];
 	for(i=0; i<count; i++)
 	{
 		[items addObject:[NSNull null]];
 	}
 
+	/*Do a reload*/
 	[list reload];
+	/*Add dividers*/
 	if(dirCount && dirCount != [_names count])
 		[list addDividerAtIndex:dirCount];
 	if(predicate != NULL && [[SapphireSettings sharedSettings] fastSwitching])
 		[list addDividerAtIndex:[_names count] -1];
+	/*Draw*/
 	[[self scene] renderScene];
 }
 
+/*!
+ * @brief Get the mode in a compatible way
+ *
+ * @return The mode selection
+ */
 - (int)selectedMode
 {
+	/*Get if using the old method*/
 	if([modeControl isKindOfClass:[BRTVShowsSortControl class]])
 		return [modeControl gimmieState] - 1;
 	
+	/*Get it from the new method*/
 	return [modeControl selectedSegment];
 }
 
@@ -203,6 +263,7 @@
     
     // always call super
     [super willBePushed];
+	/*Reload upon display*/
 	[self reloadDirectoryContents];
 }
 
@@ -212,6 +273,7 @@
     
     // always call super
     [super wasPushed];
+	/*Get metadata when we can*/
 	[metaData resumeDelayedImport];
 }
 
@@ -221,6 +283,7 @@
     
     // always call super
     [super willBePopped];
+	/*Cancel everything we were doing*/
 	[metaData cancelImport];
 	cancelScan = YES;
 	[metaData setDelegate:nil];
@@ -238,9 +301,10 @@
 {
     // The user just chose an option, and we will be taken off the screen
     
-    // always call super
+	/*Cancel everything we were doing*/
 	[metaData cancelImport];
 	cancelScan = YES;
+	// always call super
     [super willBeBuried];
 }
 
@@ -256,26 +320,29 @@
 {
     // the user pressed Menu, but we've not been revealed yet
     
+	/*Check to see if the user stopped playing something*/
 	id controller = [[self stack] peekController];
 	if([controller isKindOfClass:[BRVideoPlayerController class]])
 	{
+		/*Check for 90% completion*/
 		BRVideoPlayer *player = [(BRVideoPlayerController *)controller player];
 		float elapsed = [player elapsedPlaybackTime];
 		float duration = [player trackDuration];
 		if(elapsed / duration > 0.9f)
-		{
+			/*Mark as watched and reload info*/
 			[currentPlayFile setWatched:YES];
-			[[self list] reload];
-			[[self scene] renderScene];
-		}
+
+		/*Get the resume time to save*/
 		if(elapsed < duration - 2)
 			[currentPlayFile setResumeTime:[player elapsedPlaybackTime]];
 		else
 			[currentPlayFile setResumeTime:0];
 		[currentPlayFile writeMetaData];
 	}
+	/*cleanup*/
 	[currentPlayFile release];
 	currentPlayFile = nil;
+	/*Reload our display*/
 	[self reloadDirectoryContents];
     // always call super
     [super willBeExhumed];
@@ -287,9 +354,11 @@
     
     // always call super
     [super wasExhumedByPoppingController: controller];
+	/*Check to see if dir is empty*/
 	if(fileCount + dirCount == 0)
 		[[self stack] popController];
 	else
+		/*Resume importing now that we are up again*/
 		[metaData resumeDelayedImport];
 }
 
@@ -310,6 +379,7 @@
     return ( nil );
 */
 	NSString * displayName=nil ;
+	/*Check for no items*/
 	int nameCount = [_names count];
 	if( nameCount == 0)
 	{
@@ -319,6 +389,7 @@
 	}
 	if( row >= nameCount ) return ( nil ) ;
 	
+	/*Check our cache*/
 	id cached = [items objectAtIndex:row];
 	if(cached != [NSNull null])
 		return cached;
@@ -329,6 +400,7 @@
 	BOOL favorite = NO;
 	BOOL gear = NO;
 	BRRenderScene *scene = [self scene];
+	/*Is this a dir*/
 	if(row < dirCount)
 	{
 		result = [BRAdornedMenuItemLayer adornedFolderMenuItemWithScene: scene] ;
@@ -336,25 +408,32 @@
 		watched = [meta watchedForPredicate:predicate];
 		favorite = [meta favoriteForPredicate:predicate];
 	}
+	/*Check for a file next*/
 	else if(row < dirCount + fileCount)
 	{
 		result = [BRAdornedMenuItemLayer adornedMenuItemWithScene: scene] ;
 		SapphireFileMetaData *meta = [metaData metaDataForFile:name];
 		if(meta != nil)
 		{
+			/*Display episode number if availble*/
 			int eps= [meta episodeNumber] ;
 			displayName=[meta episodeTitle] ;
-			if(eps>0)[[result textItem] setRightJustifiedText:[NSString stringWithFormat:@" %02d",eps]];
-			else [[result textItem] setRightJustifiedText:[meta sizeString]];
+			if(eps>0)
+				[[result textItem] setRightJustifiedText:[NSString stringWithFormat:@" %02d",eps]];
+			else
+				/*Fallback to size*/
+				[[result textItem] setRightJustifiedText:[meta sizeString]];
 			watched = [meta watched];
 			favorite = [meta favorite] ;
 		}
 	}
+	/*Utility*/
 	else
 	{
 		result = [BRAdornedMenuItemLayer adornedMenuItemWithScene:scene];
 		gear = YES;
 	}
+	/*Add icons*/
 	SapphireTheme *theme = [SapphireTheme sharedTheme];
 	if(gear) [result setLeftIcon:[theme gem:GEAR_GEM_KEY]];
 	else if(!watched) [result setLeftIcon:[theme gem:BLUE_GEM_KEY]];
@@ -402,6 +481,7 @@
 	
 	if([_names count] == 0)
 	{
+		/*No items, so leave the empty dir*/
 		[[self stack] popController];
 		return;
 	}
@@ -409,13 +489,16 @@
 	NSString *name = [_names objectAtIndex:row];
 	NSString *dir = [metaData path];
 	
+	/*Check mode for mark*/
 	if([self selectedMode] == 1)
 	{
 		id meta = nil;
-		if([[metaData directories] containsObject:name])
+		/*Get metadata*/
+		if(row < dirCount)
 			meta = [metaData metaDataForDirectory:name];
 		else
 			meta = [metaData metaDataForFile:name];
+		/*Do mark menu*/
 		id controller = [[SapphireMarkMenu alloc] initWithScene:[self scene] metaData:meta];
 		[(SapphireMarkMenu *)controller setPredicate:predicate];
 		[[self stack] pushController:controller];
@@ -423,8 +506,10 @@
 		return;
 	}
 	
+	/*Check for dir*/
 	if(row < dirCount)
 	{
+		/*Browse the subdir*/
 		id controller = [[SapphireBrowser alloc] initWithScene:[self scene] metaData:[metaData metaDataForDirectory:name] predicate:predicate];
 		[controller setListTitle:name];
 		[controller setListIcon:[self listIcon]];
@@ -433,6 +518,7 @@
 	}
 	else if(row < dirCount + fileCount)
 	{
+		/*Play the video*/
 		BRVideoPlayerController *controller = [[BRVideoPlayerController alloc] initWithScene:[self scene]];
 		
 		currentPlayFile = [[metaData metaDataForFile:name] retain];
@@ -440,6 +526,7 @@
 		
 		NSString *path = [dir stringByAppendingPathComponent:name];
 		
+		/*Anonymous reporting*/
 		if(![[SapphireSettings sharedSettings] disableAnonymousReporting])
 		{
 			NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:@"http://appletv.nanopi.net/show.php"]];
@@ -451,19 +538,23 @@
 			[request setValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"Content-Type"];
 			[request setHTTPBody:postData];
 			[request setCachePolicy:NSURLRequestReloadIgnoringCacheData];
+			/*Trigger the req*/
 			NSURLDownload *download = [[NSURLDownload alloc] initWithRequest:request delegate:nil];
 			[download autorelease];
 		}
 		
+		/*Set the asset resume time*/
 		NSURL *url = [NSURL fileURLWithPath:path];
 		SapphireMedia *asset  =[[SapphireMedia alloc] initWithMediaURL:url];
 		[asset setResumeTime:[currentPlayFile resumeTime]];
 
+		/*Get the player*/
 		SapphireVideoPlayer *player = [[SapphireVideoPlayer alloc] init];
 		[player setMetaData:currentPlayFile];
 		NSError *error = nil;
 		[player setMedia:asset error:&error];
 		
+		/*and go*/
 		[controller setVideoPlayer:player];
 		[[self stack] pushController:controller];
 
@@ -473,16 +564,27 @@
 	}
 	else
 	{
+		/*Do a scan*/
 		cancelScan = NO;
 		[metaData scanForNewFilesWithDelegate:self];
 	}
 }
 
+/*!
+ * @brief Finished scanning the dir
+ *
+ * @param subs nil in this case
+ */
 - (void)gotSubFiles:(NSArray *)subs
 {
 	[self reloadDirectoryContents];	
 }
 
+/*!
+ * @brief Check to see if the scan should be canceled
+ *
+ * @return YES if the scan should be canceled
+ */
 - (BOOL)getSubFilesCanceled
 {
 	return cancelScan;
@@ -494,15 +596,18 @@
     // passes over an item.
 	if(row >= [_names count])
 		return nil;
+	/*Check to see if it is a dir or file*/
 	NSString *name = [_names objectAtIndex:row];
 	if(row < dirCount + fileCount)
 	{
 		SapphireMediaPreview *preview = [[SapphireMediaPreview alloc] initWithScene:[self scene]];
+		/*Check for dir*/
 		if(row < dirCount)
 			[preview setMetaData:[metaData metaDataForDirectory:name]];
 		else
 			[preview setMetaData:[metaData metaDataForFile:name]];
 		[preview setShowsMetadataImmediately:NO];
+		/*And go*/
 		return [preview autorelease];
 	}
     return ( nil );
@@ -510,15 +615,23 @@
 
 - (BOOL)brEventAction:(id)fp8
 {
+	/*Cancel imports on an action*/
 	[metaData resumeDelayedImport];
 	return [super brEventAction:fp8];
 }
 
+/*!
+ * @brief The import on a file completed
+ *
+ * @param file Filename to the file which completed
+ */
 - (void)updateCompleteForFile:(NSString *)file
 {
+	/*Get the file*/
 	int index = [_names indexOfObject:file];
 	if(index != NSNotFound)
 		[items replaceObjectAtIndex:index withObject:[NSNull null]];
+	/*Relead the list and render*/
 	BRListControl *list = [self list];
 	[list reload];
 	[[self scene] renderScene];
--- SapphireMedia.m
+++ SapphireMedia.m
@@ -11,15 +11,27 @@
 
 @implementation SapphireMedia
 
+/*!
+ * @brief Set the resume time for the media
+ *
+ * @param time the time at which to resume
+ */
 - (void)setResumeTime:(unsigned int)time
 {
 	resumeTime = time;
 }
 
+/*!
+ * @brief Overrides the bookmark time
+ *
+ * @return The resume time if exists, otherwise the bookmark time
+ */
 - (unsigned int)bookmarkTimeInSeconds
 {
+	/*Check for resume time and if none, return bookmark time*/
 	if(time == 0)
 		return [super bookmarkTimeInSeconds];
+	/*return resume time*/
 	return resumeTime;
 }
 
