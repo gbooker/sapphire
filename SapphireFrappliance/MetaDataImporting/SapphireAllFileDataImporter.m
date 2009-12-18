cda0ddf1d15515872143e63669f91b9115a8d0e6
gbooker <gbooker@7e1782f7-7b3f-45d9-b215-e5100ed717cc>
2009-12-18 02:23:56 +0000

    Add back the calls to super since it makes the compiler shut up.
    git-svn-id: https://appletv.nanopi.net/svn/trunk@985 7e1782f7-7b3f-45d9-b215-e5100ed717cc

--- SapphireFrappliance/MetaDataImporting/SapphireAllFileDataImporter.m
+++ SapphireFrappliance/MetaDataImporting/SapphireAllFileDataImporter.m
@@ -59,4 +59,24 @@
 	return BRLocalizedString(@"Start Populating Data", @"Button");
 }
 
+- (void)exhumedChooser:(BRLayerController <SapphireChooser> *)chooser withContext:(id)context
+{
+	[super exhumedChooser:chooser withContext:context];
+}
+
+- (void)cancelImports
+{
+	[super cancelImports];
+}
+
+- (void)setDelegate:(id <SapphireImporterDelegate>)aDelegate
+{
+	[super setDelegate:aDelegate];
+}
+
+- (ImportState)importMetaData:(SapphireFileMetaData *)metaData path:(NSString *)path
+{
+	return [super importMetaData:metaData path:path];
+}
+
 @end
