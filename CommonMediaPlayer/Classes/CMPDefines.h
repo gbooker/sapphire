5655e23517aa19c3a6d3ddf1d1a23b175f73e770
gbooker <gbooker@7e1782f7-7b3f-45d9-b215-e5100ed717cc>
2010-10-02 19:09:44 +0000

    Forgot to increment version after last commit
    git-svn-id: https://appletv.nanopi.net/svn/trunk@1327 7e1782f7-7b3f-45d9-b215-e5100ed717cc

--- CommonMediaPlayer/Classes/CMPDefines.h
+++ CommonMediaPlayer/Classes/CMPDefines.h
@@ -21,5 +21,5 @@
 
 //Note, only defines should ever be in this file!
 
-#define CMPVersion 11  //Full Version Number
+#define CMPVersion 12  //Full Version Number
 #define CMPAPIVersion 1 //API version 1
